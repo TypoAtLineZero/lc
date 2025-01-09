f6ca0b4f55d53ccd79dd8bcc1ae851f5276c1e6c
franz <fneumeister@in-technology.eu>
2025-01-09 22:44:23 +0100

    Adding second solution to 80_

--- 80_RemoveDuplicatesFromSortedArrayII.cpp
+++ 80_RemoveDuplicatesFromSortedArrayII.cpp
@@ -1,6 +1,41 @@
+#include <vector>
+
 class Solution {
 public:
-    int removeDuplicates(vector<int>& nums) {
-        
+    int removeDuplicates(std::vector<int>& nums) {
+        // Solution 1
+        //int k = 0;
+        //for (int i = 0; i < nums.size(); ++i) {
+        //    if (i < 2 || nums[i] != nums[k - 2]) {
+        //        nums[k++] = nums[i];
+        //    }
+        //}
+        //return k;
+
+        // Solution 2
+        int j = 1;
+        int count = 1;
+        int n = nums.size();
+
+        for (int i = 1; i < n; ++i) {
+            if (nums[i] == nums[i - 1]) {
+                count += 1;
+            } else {
+                count = 1;
+            }
+
+            if (count <= 2) {
+                nums[j] = nums[i];
+                j += 1;
+            }
+        }
+        return j;
     }
-};
+};       
+
+int main(void) {
+    std::vector<int> nums1 = {1,1,2,2,2,3};
+
+    Solution one;
+    one.removeDuplicates(nums1);
+}
