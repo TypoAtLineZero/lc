85df2e48d7f3739d7cb0f553905716edf23f4141
franz <fneumeister@in-technology.eu>
2025-01-09 12:20:01 +0100

    Adding solution to 26_RemoveDuplicatesFromArray

--- 26_RemoveDuplicatesFromSortedArray.cpp
+++ 26_RemoveDuplicatesFromSortedArray.cpp
@@ -1,6 +1,22 @@
+#include <vector>
+
 class Solution {
 public:
-    int removeDuplicates(vector<int>& nums) {
-        
+    int removeDuplicates(std::vector<int>& nums) {
+        int k = 0;
+        for (int i = 1; i < nums.size(); ++i) {
+            if (nums[k] != nums[i]) {
+                ++k;
+                nums[k] = nums[i];
+            }
+        }
+        return k + 1;
     }
 };
+
+int main(void) {
+    std::vector<int> nums1 = {0,0,1,1,1,2,2,3,3,4};
+
+    Solution one;
+    one.removeDuplicates(nums1);
+}
