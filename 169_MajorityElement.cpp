ab542993961d770ceafe34a93b7cc8540de9b884
franz <fneumeister@in-technology.eu>
2025-01-11 23:53:02 +0100

    Adding approach for 169

--- 169_MajorityElement.cpp
+++ 169_MajorityElement.cpp
@@ -1,6 +1,21 @@
+#include <unordered_map>
+#include <vector>
+
 class Solution {
 public:
-    int majorityElement(vector<int>& nums) {
-        
+    int majorityElement(std::vector<int>& nums) {
+        std::unordered_map<int, int> umap;
+
+        for (int i = 0; i < nums.size(); ++i) {
+            if (nums.find(nums[i]) == nums.end()) {
+                
+            } 
     }
 };
+
+int main(void) {
+    Solution n;
+    std:vector<int> nums = {1,2,1};
+
+    n.majorityElement(nums);
+}
