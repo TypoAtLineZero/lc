c8f20ef45fcbc1ce1a395e859d1e3279064e04b3
franz <fneumeister@in-technology.eu>
2025-01-16 13:18:10 +0100

    Init 122_

---
+++ 122_BestTimeToBuyAndSellStockII.cpp
@@ -0,0 +1,38 @@
+#include <vector>
+#include <iostream>
+
+class Solution {
+public:
+    int maxProfit(std::vector<int>& prices) {
+        if (prices.size() < 2) {
+            return 0;
+        }
+
+        int buy = prices[0];
+        int profit = 0;
+        //bool isDecreasing = true;
+        for (int price : prices) {
+            // Range based 
+            std::cout << price << std::endl;
+            if (price < buy) {
+                buy = price;
+            } else {
+                int current_profit = price - buy;
+                if (current_profit > profit) {
+                    profit = current_profit;
+                }
+            }
+        }
+        //if (isDecreasing) {
+        //    return 0;
+        //}
+        return profit;
+    }
+};
+
+int main(void) {
+    Solution n;
+    std::vector<int> nums = {1,2,4};
+
+    n.maxProfit(nums);
+}
