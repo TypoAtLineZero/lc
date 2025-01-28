0bebfdcb24043831f4cd7db6a6140b278d5bbf86
franz <fneumeister@in-technology.eu>
2025-01-28 23:18:27 +0100

    Adding solution to 45_

--- 45_JumpGameII.cpp
+++ 45_JumpGameII.cpp
@@ -2,25 +2,22 @@
 
 class Solution {
 public:
-    bool canJump(std::vector<int>& nums) {
-    int maxReach = 0; // Tracks the farthest index you can reach
-    int n = nums.size();
-
-    for (int i = 0; i < n; ++i) {
-        if (i > maxReach) {
-            // If the current index is beyond the farthest reachable index, you're stuck
-            return false;
+    int jump(std::vector<int>& nums) {    
+        int numJumps = 0;           // Number of jumps made.
+        int currentMaxReach = 0;    // Max index we can reach with the current number of jumps.
+        int lastMaxReach = 0;       // Max index we can reach from the last jump.
+        // Iterate over each element in the array except the last one.
+        for (int i = 0; i < nums.size() - 1; ++i) {
+            // Update the furthest index we can reach from here.
+            currentMaxReach = max(currentMaxReach, i + nums[i]);
+            if (lastMaxReach == i) {
+                numJumps++;
+                lastMaxReach = currentMaxReach;  // Update the max reach for the new jump.
+            }
         }
-        maxReach = std::max(maxReach, i + nums[i]); // Update the farthest reachable index
-        if (maxReach >= n - 1) {
-            // If we can reach or exceed the last index, return true
-            return true;
-        }
-    }
-    return false; // If we exit the loop without returning, we cannot reach the end
+        return numJumps;  // Return the total number of jumps made to reach the end.
     }
 };
-
 int main(void) {
     Solution n;
     std::vector<int> nums = {1,2,4};
