50aa0e571d9def30a1e72b759ab3e189e67a55c4
franz <fneumeister@in-technology.eu>
2025-02-13 21:02:00 +0100

    Adding solution for 238_

--- 238_ProductOfArrayExceptSelf.cpp
+++ 238_ProductOfArrayExceptSelf.cpp
@@ -3,10 +3,30 @@
 class Solution {
 public:
     std::vector<int> productExceptSelf(std::vector<int>& nums) {
-        
+        int n = nums.size();
+        std::vector<int> answer(n, 1);
+
+        // Compute prefix products
+        int prefix = 1;
+        for (int i = 0; i < n; ++i) {
+            answer[i] = prefix;
+            prefix *= nums[i];
+        }
+
+        // Compute suffix products and multiply with prefix products
+        int suffix = 1;
+        for (int i = n - 1; i >= 0; --i) {
+            answer[i] *= suffix;
+            suffix *= nums[i];
+        }
+
+        return answer;
     }
 };
 
-int main(void) {
 
+int main(void) {
+    Solution sol;
+    std::vector<int> input = {1,2,3,4};
+    std::vector answer = sol.productExceptSelf(input);
 }
