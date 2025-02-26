ee77757ff4974d7a9e94926da79e20cb4d018ecb
franz <fneumeister@in-technology.eu>
2025-02-26 20:33:41 +0100

    Adding solution for 135_

--- 135_Candy.cpp
+++ 135_Candy.cpp
@@ -1,7 +1,28 @@
+#include <vector>
+#include <numeric>
+
 class Solution {
 public:
-    int candy(vector<int>& ratings) {
+    int candy(std::vector<int>& ratings) {
+        int n = ratings.size();
+        std::vector<int> candies(n, 1);
         
+        int last = ratings[0];
+
+        for(int i = 1; i < n; ++i){
+            if(ratings[i] > ratings[i-1]){
+                candies[i] = candies[i-1]+1;
+            }
+        }
+
+        for(int i = n-2; i >= 0; --i){
+            if(ratings[i] > ratings[i+1]){
+                //here candies[i] could > candies[i+1], so we need the max(..., ...) here
+                candies[i] = std::max(candies[i], candies[i+1]+1);
+            }
+        }
+
+        return std::accumulate(candies.begin(), candies.end(), 0);
     }
 };
 
